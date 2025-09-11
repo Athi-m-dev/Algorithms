922fcbcb4e708e6a46acd6cf31442ff9bde5e6e4
Athithiyan-arch <athithiyanm87@gmail.com>
2025-09-11 14:45:54 +0530

    sive of algorithm

---
+++ siveofprime.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class siveofprime {
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        int n = obj.nextInt();
+        int mid = (n - 1) / 2;
+        boolean[] mark = new boolean[mid + 1];
+
+        for (int i = 1; i <= mid; i++) {
+            for (int j = i; (i + j + 2 * i * j) <= mid; j++) {
+                mark[i + j + 2 * i * j] = true;
+            }
+        }
+        System.out.println("The prime Number up to n is");
+        if (n > 2) {
+            System.out.print("2" + " ");
+        }
+
+        for (int k = 1; k <= mid; k++) {
+            if (mark[k] == false) {
+                System.out.print((2 * k + 1) + " ");
+            }
+        }
+    }
+}
+
+// Time Complecity - O(mid2) + O(mid)
+// space Complecity - O(mark)
